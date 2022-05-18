7e406433b88b0db853d69ae40c440d8577b6cb41
YuriMacBook <07yuri@gmail.com>
2022-05-18 16:42:20 -0400

    Add conditional statements to display different components based on props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './styles.scss';
+import Header from './Header';
+import Show from './Show';
+
+import Empty from './Empty';
+import Form from './Form';
+import Confirm from './Confirm';
+import Status from './Status';
+import Error from './Error';
+
 
 const Appointment = (props) => {
   return (
-    <article className="appointment"></article>
+    <Fragment>
+      <Header time={props.time}/>
+      { props.interview ?
+      <>
+      <article className="appointment">
+        <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
+      </article>
+      </>
+      :
+      <>
+      <article className="appointment">
+        <Empty/>
+      </article>
+      </>
+      }
+    </Fragment>
   );
 };
 
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment }from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -174,7 +174,7 @@ storiesOf("Appointment", module)
     student={"Lydia Miller-Jones"} 
     interviewer={2} 
     interviewers={interviewers}
-    onSave={action({"onSave"})}
+    onSave={action("onSave")}
     onCancel={action("onCancel")}
     />)
   .add("Create", () => 
@@ -183,3 +183,18 @@ storiesOf("Appointment", module)
     onSave={action("onSave")}
     onCancel={action("onCancel")}
     />)
+  .add("Appointment Empty", () =>
+    <Fragment>
+      <Appointment id={1} time={"4pm"}/>
+      <Appointment time={"5pm"}/>
+    </Fragment>)
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
