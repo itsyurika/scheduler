2c465723bd4171afb52201f468292b69e632b1c3
YuriMacBook <07yuri@gmail.com>
2022-05-18 17:00:42 -0400

    import appointment component into the application compoment

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,9 +22,57 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-
+  
+  const parsedAppointments = Object.values(appointments).map((appointment)=> {
+    return(
+      <Appointment 
+        key={appointment.id}
+        {...appointment}
+      />
+    )
+  })
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,7 +96,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {parsedAppointments}
+        <Appointment key="last" time="5pm"/> 
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import './styles.scss';
 import Header from './Header';
 import Show from './Show';
-
 import Empty from './Empty';
+
 import Form from './Form';
 import Confirm from './Confirm';
 import Status from './Status';
